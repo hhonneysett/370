0adeecc8255f626f3179537b3ed4de360479ecbe
Bennytenny <ben.botha3@gmail.com>
2016-08-20 21:52:23 +0200

    Question Bank mostly done

---
+++ LibraryAssistantApp/LibraryAssistantApp/Models/QuestionTopic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LibraryAssistantApp.Models
+{
+    [MetadataType(typeof(Question_TopicMetadata))]
+    public partial class Question_Topic
+    {
+    }
+
+    public class Question_TopicMetadata
+    {
+        [Display(Name = "Topic")]
+        public string Topic_Name;
+
+        [Display(Name = "Topic")]
+        public int Topic_Seq { get; set; }
+
+
+    }
+}
